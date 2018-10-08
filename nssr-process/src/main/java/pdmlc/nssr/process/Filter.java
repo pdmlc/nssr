253e2cf02f02b81bfc5864d81919f2497fd2c8d2
Scott Thiel <scott@scottnotfound.net>
2018-10-08 17:22:52 -0400

    filter by solvent added

--- nssr-process/src/main/java/pdmlc/nssr/process/Filter.java
+++ nssr-process/src/main/java/pdmlc/nssr/process/Filter.java
@@ -7,16 +7,25 @@ public class Filter {
 
     public static void main(String[] args) throws IOException {
 
+        String record = "all_records_";
+
         Filter filter = new Filter();
-        filter.filterBySpectrum("all_records_", "13C");
-        filter.filterBySpectrum("all_records_", "1H");
-        filter.filterBySpectrum("all_records_", "17O");
-        filter.filterBySpectrum("all_records_", "31P");
+
+        filter.filterBySpectrum(record, "13C");
+        filter.filterBySpectrum(record, "1H");
+        filter.filterBySpectrum(record, "17O");
+        filter.filterBySpectrum(record, "31P");
+
+        filter.filterBySolvent(record, "Chloroform-D1 (CDCl3)");
 
     }
 
     boolean filterSpectrum(String object, String spectrum) {
         return object.contains("\"spectrum\":\""+spectrum+"\"");
+    }
+
+    boolean filterSolvent(String object, String solvent) {
+        return object.contains("\"solvent\":\""+solvent+"\"");
     }
 
     void filterBySpectrum(String in, String spectrum) throws IOException {
@@ -30,6 +39,27 @@ public class Filter {
         BufferedReader i = new BufferedReader(new InputStreamReader(Files.newInputStream(ifile.toPath())));
         i.lines()
          .filter(obj -> filterSpectrum(obj, spectrum))
+         .forEach(str -> {
+             try {
+                 o.write(str+'\n');
+             } catch (IOException e) {
+                 e.printStackTrace();
+             }
+         });
+
+    }
+
+    void filterBySolvent(String in, String solvent) throws IOException {
+
+        String out = in + "filtered_" + solvent;
+
+        File ifile = new File("records/"+in+".json");
+        File oFile = new File("records/filtered/"+out+".json");
+
+        BufferedWriter o = new BufferedWriter(new OutputStreamWriter(Files.newOutputStream(oFile.toPath())));
+        BufferedReader i = new BufferedReader(new InputStreamReader(Files.newInputStream(ifile.toPath())));
+        i.lines()
+         .filter(obj -> filterSolvent(obj, solvent))
          .forEach(str -> {
              try {
                  o.write(str+'\n');
